3be84c6dc526a636d8d4e0114cf28666a3163f54
96paradise <cnw0806@naver.com>
2025-04-02 16:10:05 +0900

    20250402 CNW

--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import UseState01 from "./a_useState/UseState01";
 import UseState02 from "./a_useState/UseState02";
 import UseState03 from "./a_useState/UseState03";
@@ -6,9 +7,10 @@ import UseState04 from "./a_useState/UseState04";
 import UseState05 from "./a_useState/UseState05";
 import UseState06 from "./a_useState/UseState06";
 import UseStatePractice from "./a_useState/Practice";
+
 import UseRef01 from "./b_useRef/UseRef01";
 import UseRef02 from "./b_useRef/UseRef02";
-import Practice from "./b_useRef/Practice";
+import UseRefPractice from "./b_useRef/Practice";
 import UseEffect01 from "./c_useEffect/UseEffect01";
 import UseEffect02 from "./c_useEffect/UseEffect02";
 import UseCallback from "./d_useCallback_useMemo/UseCallback";
@@ -16,6 +18,9 @@ import UseMemo from "./d_useCallback_useMemo/UseMemo";
 import ReactMemo from "./d_useCallback_useMemo/ReactMemo";
 import UseReducer01 from "./e_useReducer/UseReducer01";
 import UseReducer02 from "./e_useReducer/UseReducer02";
+import Custom01 from "./f_customHook/Custom01";
+import Custom02 from "./f_customHook/Custom02";
+import TodoAppLocalStorage from "./z_todo/TodoAppLocalStorage";
 
 const h2Style = {
   backgroundColor: "black",
@@ -33,10 +38,28 @@ function Index() {
       >
         === 리액트 Hooks ===
       </h1>
+
+      <h2 style={h2Style}>리액트 Hooks - Todo 앱 구현</h2>
+      <div style={{
+        margin: '20px auto',
+        padding: '10px',
+        height: '100vh',
+        backgroundColor: '#f4f4f4',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <TodoAppLocalStorage />
+      </div>
+
+      <h2 style={h2Style}>리액트 Hooks - 커스텀 훅</h2>
+      <Custom01 />
+      <Custom02 />
+
       <h2 style={h2Style}>리액트 Hooks - useReducer</h2>
       <UseReducer01 />
       <UseReducer02 />
-
+      
       <h2 style={h2Style}>리액트 Hooks - useCallback & useMemo</h2>
       <UseCallback />
       <UseMemo />
@@ -49,7 +72,7 @@ function Index() {
       <h2 style={h2Style}>리액트 Hooks - useRef</h2>
       <UseRef01 />
       <UseRef02 />
-      <Practice />
+      <UseRefPractice />
 
       <h2 style={h2Style}>리액트 Hooks - useState</h2>
       <UseState01 /> <hr />
@@ -58,7 +81,7 @@ function Index() {
       <UseState04 /> <hr />
       <UseState05 /> <hr />
       <UseState06 /> <hr />
-      <UseStatePractice /> <hr />
+      <UseStatePractice />
 
     </div>
   );
