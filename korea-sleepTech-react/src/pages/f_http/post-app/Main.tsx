2e08e4161e5e0d216895f0caa50864725ee51042
96paradise <cnw0806@naver.com>
2025-04-08 15:40:06 +0900

    20250408 CNW

--- korea-sleepTech-react/src/pages/f_http/post-app/Main.tsx
+++ korea-sleepTech-react/src/pages/f_http/post-app/Main.tsx
@@ -4,15 +4,14 @@ import PostList from './components/PostList'
 
 /*
 # React(TS) + Axios + localStorage(저장소)
-
-리액트 >> 로컬 스토리지(게시글 CRUD)
+리액트 >> 로컬 스토리지 (게시글 CRUD)
 
 ! 요구사항
 : 게시글 CRUD
 
 ! 프로젝트 폴더 구조
 post-app
-> apis / postApi.ts
+> apis 폴더 / postApi.ts
 > components 폴더 / PostForm.tsx, PostList.tsx
 
 > Main.tsx
@@ -20,10 +19,10 @@ post-app
 
 function Main() {
   return (
-    <div style={{padding: '20px'}}>
-      <h1>게시판 CRUD 예제(Axios/localStorage)</h1>
+    <div style={{ padding: '20px' }}>
+      <h1>게시판 CRUD 예제 (Axios/localStorage)</h1>
       <PostForm />
-        <hr />
+      <hr />
       <PostList />
     </div>
   )
